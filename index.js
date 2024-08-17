3242129063ef68574858f6e857cd9832bf6fdfe3
daksh182508 <daksh182508@gmail.com>
2024-08-17 10:10:45 +0530

    everything uploaded

---
+++ index.js
@@ -0,0 +1,30 @@
+let pss = document.getElementById("pss")
+let hi =" chat GPT is an AI language model developed by OpenAI, based on the GPT-4 architecture.  My primary function is to understand and generate human-like text based on the prompts or questions I'm given.I'm designed to assist with a wide range of tasks, from answering questions and providing explanations to helping with writing, brainstorming, tutoring, and more.I don't have personal experiences, consciousness, or emotions, but I've been trained on a large dataset of text,which allows me to provide information and engage in conversations on a variety of topics.My knowledge is up-to-date as of 2023, and I can assist with subjects like science, history, literature,  technology, and many others."
+let pro = "Siri is a virtual assistant developed by Apple Inc.It uses voice recognition and natural language processing to respond to user queries,perform tasks, and offer recommendations. Siri was first introduced as an app for iOS in 2010, and later became a built-in feature of Apple's ecosystem, available on iPhones, iPads, Macs, Apple Watches, and more."
+let noob = "Alexa is a virtual assistant developed by Amazon. It was first introduced with the Amazon Echo smart speaker in 2014and has since become a central part of Amazon's smart home ecosystem."
+let bot = " Google Assistant is a virtual assistant developed by Google.It uses voice recognition and natural language processing to interact with users,answer questions, perform tasks, and provide recommendations.Google Assistant was first launched in 2016 as part of Google's messaging app, Allo,and later became integrated into Android devices, smart speakers, smart displays, and more."
+let me = "Cortana is a virtual assistant developed by Microsoft.It was initially released in 2014 as part of the Windows Phone 8.1 update.Cortana was later integrated into Windows 10, Xbox, and other Microsoft devices."
+let dks = document.getElementById("dks")
+let toy = "this website is programmed by Mr Divyanshu and it is about the well growing technology artificial inteligence(Ai). this website provides us knowledge about ai's basic and about the startting of ai ."
+
+function tol(){
+   pss.textContent = hi
+}
+
+function toll(){
+    pss.textContent = pro
+}
+
+function car(){
+    pss.textContent = noob
+}
+
+function ggle(){
+    pss.textContent = bot
+}
+function ctna(){
+    pss.textContent = me
+}
+function fan(){
+    dks.textContent = toy
+}
